386ea786e5c8fbee7dd66f2171913121101ab3dc
Rama Krishna Oguri <ramakrishna.oguri@gmail.com>
2024-07-17 18:35:26 +0100

    Changing Desk position according to table anchor

--- Assets/Scripts/StartMenuUiCtrl.cs
+++ Assets/Scripts/StartMenuUiCtrl.cs
@@ -1,3 +1,4 @@
+using Meta.XR.MRUtilityKit;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,42 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuUiCtrl : MonoBehaviour
 {
+    public Transform desk;
+    private Vector3 tableOffset = new Vector3(0f, 1.037f, 0f);
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += AttachDeskToTheTable;
+    }
+
     public void OnClickPlayBtn()
     {
         SceneManager.LoadScene("01Main");
     }
+    private void AttachDeskToTheTable(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.name == "00Start")
+        {
+            MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+            MRUKAnchor tableAnchor = room.Anchors.Find((tableAnchor) =>
+            {
+                return tableAnchor.HasLabel("TABLE");
+
+            });
+            if (tableAnchor != null)
+            {
+                desk.position = tableAnchor.transform.position + tableOffset;
+                desk.up = tableAnchor.transform.forward;
+                desk.forward = tableAnchor.transform.right;
+                //desk.Lo
+                //desk.rotation = tableAnchor.transform.rotation;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= AttachDeskToTheTable;
+    }
 }
+
